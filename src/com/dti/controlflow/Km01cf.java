48328d3c156a6a3eb03e7756739df47e1f6f3b76
Sujitra141 <s6752C10001@sau.ac.th>
2025-01-19 18:08:48 +0700

    v.1 create Km01CF

---
+++ src/com/dti/controlflow/Km01cf.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dti.controlflow;
+
+/**
+ *
+ * @author IT306PC
+ */
+public class Km01cf {
+    public static void main(String[] args) {
+        
+    }
+}
